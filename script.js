f22f2c551f4b13e57db0f84d51f322ab2caba096
jiwansapkota <jwnspkt@gmail.com>
2021-08-09 11:36:02 +0545

    changed request to get

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+
 /**
  * @Created 28/07/2021 - 22:56 PM
  * @Project ipo-result-client
  * @Author Jiwan Sapkota - sapkotazeewan13@gmail.com
  */
+const baseUrl = "http://localhost:4099/api"
 const wrapper = document.querySelector('.share__allotment');
 form = wrapper.querySelectorAll('.form');
 submitInput = form[0].querySelector('input[type="submit"]');
@@ -10,38 +12,79 @@ const btn = document.getElementById("form__btn");
 const error = document.getElementById("error_boid");
 const companyError = document.querySelector('#error_company');
 
- postData=(e)=> {
+function getCompanies() {
+    const request = new XMLHttpRequest();
+    request.open("GET", `${baseUrl}/companies`);
+    request.onerror=()=>{
+        result.style["padding-top"] = "2rem";
+        result.style.color = "#ed1c24";
+        console.log(request.error);
+        result.innerHTML = "can't connect to server";
+    }
+    request.onload = () => {
+        const responseBody = JSON.parse(request.response);
+        result.style.color = "#010101";
+        result.style["padding-top"] = "2rem";
+        result.innerHTML = responseBody.msg;
+        const select = document.getElementById('companyName');
+        responseBody.data.forEach(element => {
+            const opt = document.createElement('option');
+            opt.value=element;
+            opt.innerHTML = element;
+            select.appendChild(opt);
+        });
+    }
+    request.send();
+};
+window.onload = function () {
+    getCompanies();
+}
+
+getData = (e) => {
     e.preventDefault();
     const result = document.getElementById("result");
     const boid = document.querySelector('#boid');
     const company = document.getElementById("companyName");
     btn.disabled = true;
-    if (boid.value.length!=16 && company.value=="") {
-        boid.style["border-color"]="#ed1c24";
-        company.style["border-color"]="#ed1c24";
+    if (boid.value.length != 16 && company.value == "") {
+        boid.style["border-color"] = "#ed1c24";
+        company.style["border-color"] = "#ed1c24";
         error.innerHTML = "Please enter your 16 digit BOID number.";
         companyError.innerHTML = "You need to select a company.";
         return
     }
-    if (boid.value.length!=16) {
-        boid.style["border-color"]="#ed1c24";
+    if (boid.value.length != 16) {
+        boid.style["border-color"] = "#ed1c24";
         error.innerHTML = "Please enter your 16 digit BOID number.";
         return
     }
-    if (company.value=="") {
-        company.style["border-color"]="#ed1c24";
+    if (company.value == "") {
+        company.style["border-color"] = "#ed1c24";
         companyError.innerHTML = "You need to select a company.";
         return
     }
-    btn.value='Checking...'
+    btn.value = 'Checking...'
     const reqBody = {
         boid: boid.value,
         company: company.value
     };
     const request = new XMLHttpRequest();
-    request.open("POST", "http://65.1.59.134:4099/result");
-    request.setRequestHeader('Content-type', 'application/json');
-    request.send(JSON.stringify(reqBody));
+    // get request 
+    request.open("GET", `${baseUrl}/result?boid=${boid.value}&company=${company.value}`);
+    request.send();
+
+    //if post request 
+    // request.open("POST", `${baseUrl}/result`);
+    // request.setRequestHeader('Content-type', 'application/json');
+    // request.send(JSON.stringify(reqBody));
+    request.onerror=()=>{
+        result.style.color = "#ed1c24";
+        console.log('response is:',request);
+        result.style["padding-top"] = "2rem";
+        result.innerHTML = "network error, please check your internet connection";
+        btn.disabled=false;
+        btn.value = 'Check';
+    }
     request.onload = () => {
         const responseBody = JSON.parse(request.response);
         result.style["padding-top"] = "2rem";
@@ -51,7 +94,7 @@ const companyError = document.querySelector('#error_company');
         else {
             result.style.color = "#010101";
         }
-        btn.value='Check';
+        btn.value = 'Check';
         result.innerHTML = responseBody.msg;
     }
 }
@@ -60,36 +103,36 @@ document.querySelector('#companyName').addEventListener('input', function () {
     btn.disabled = false;
     result.innerHTML = '';
     if (this.value == "") {
-        this.style["border-color"]="#ed1c24";
+        this.style["border-color"] = "#ed1c24";
         companyError.innerHTML = "You need to select a company.";
     }
-    else{
-        this.style["border-color"]="#515e63";
+    else {
+        this.style["border-color"] = "#515e63";
         companyError.innerHTML = "";
     }
 });
 
 document.querySelector('#boid').addEventListener('keypress', function (e) {
     if (e.key === 'Enter') {
-        postData();
+        getData();
     }
 });
 
 document.querySelector('#boid').addEventListener('input', function () {
     if (this.value.length > 16)
-        this.value = this.value.slice(0,16); 
+        this.value = this.value.slice(0, 16);
     btn.disabled = false;
     result.innerHTML = '';
-    if(this.value.length!=16){
-        this.style["border-color"]="#ed1c24";
+    if (this.value.length != 16) {
+        this.style["border-color"] = "#ed1c24";
         error.innerHTML = 'Please enter your 16 digit BOID number.';
     }
-    if (this.value.length==16) {
-        this.style["border-color"]="#515e63";
+    if (this.value.length == 16) {
+        this.style["border-color"] = "#515e63";
         error.innerHTML = '';
     }
 });
 
 document.addEventListener('DOMContentLoaded', function () {
-    submitInput.addEventListener('click', postData, false)
+    submitInput.addEventListener('click', getData, false)
 }, false);
